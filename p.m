ce08bd0f8442eb56a5f21c2610fa848c6aa82ca2
arbuzovada <arbuzovada@gmail.com>
2014-02-19 14:01:00 +0400

    p(c), p(d) and improvements for p(c|a, b), p(d|c)

--- p.m
+++ p.m
@@ -8,26 +8,27 @@ function res = p(s, arg1, arg2, arg3, arg4)
 % OUTPUT:
 %    p: double, probability
 
-    res = 0.5;
+    res = NaN;
+    if (s == 'a')
+        res = p_a(arg1);
+    elseif (s == 'b')
+        res = p_b(arg1);
+    elseif (s == 'c')
+        res = p_c(arg1);
+    elseif (s == 'd')
+        res = p_d(arg1);
+    end
 end
-
-% CONSTANTS
-% A_MIN = 15;
-% A_MAX = 30;
-% B_MIN = 250;
-% B_MAX = 350;
-% P_1 = 0.5;
-% P_2 = 0.05;
-% P_3 = 0.5;
 
 function res = p_a(a)
 % This function returns the probability p(a)
 % INPUT:
-%    a: double
+%    a: int
 %
 % OUTPUT:
 %    res: double, p(a)
 
+    global A_MIN A_MAX B_MIN B_MAX P_1 P_2 P_3 N M
     res = 0;
     if (a >= A_MIN) || (a <= A_MAX)
         res = 1 / (A_MAX - A_MIN + 1);
@@ -37,11 +38,12 @@ end
 function res = p_b(b)
 % This function returns the probability p(b)
 % INPUT:
-%    b: double
+%    b: int
 %
 % OUTPUT:
 %    res: double, p(b)
 
+    global A_MIN A_MAX B_MIN B_MAX P_1 P_2 P_3 N M
     res = 0;
     if (b >= B_MIN) && (b <= B_MAX)
         res = 1 / (B_MAX - B_MIN + 1);
@@ -51,33 +53,105 @@ end
 function res = p_c_cond_a_b(c, a, b)
 % This function returns the probability p(c | a, b)
 % INPUT:
-%    c: double
-%    a: double
-%    b: double
+%    c: int
+%    a: 1-by-n array of int
+%    b: 1-by-m array of int
 %
 % OUTPUT:
-%    res: double, p(c | a, b)
+%    res: n-by-m array of double, p(c | a, b)
     
+    global A_MIN A_MAX B_MIN B_MAX P_1 P_2 P_3 N M
     res = 0;
-    if (a >= A_MIN) && (a <= A_MAX) && (b >= B_MIN) && (b <= B_MAX) && ...
-        (c >= 0) && (c <= a + b)
-        % lambda = a * P1 + b * P2;
-        % res = exp(-lambda) * lambda ^ c / factorial(c);
-        res = poisspdf(c, a * P_1 + b * P_2);
+    if all(a >= A_MIN) && all(a <= A_MAX) && all(b >= B_MIN) && all(b <= B_MAX) && ...
+        (c >= 0) && (c <= max(a) + max(b))
+        m = size(b, 2);
+        n = size(a, 2);
+%         size(repmat(a', 1, m))
+%         size(repmat(b, n, 1) * P_2)
+        lambda = repmat(a', 1, m) * P_1 + repmat(b, n, 1) * P_2;
+        res = exp(-lambda) .* lambda .^ c / factorial(c);
+        if isnan(res)
+            fprintf('fail\n');
+        end
+        %res = poisspdf(c, a * P_1 + b * P_2);
     end
 end
 
 function res = p_d_cond_c(d, c)
 % This function returns the probability p(d | c)
 % INPUT:
-%    d: double
-%    c: double
+%    d: int
+%    c: 1-by-n array of int
 %
 % OUTPUT:
-%    res: double, p(d | c)
+%    res: 1-by-n array of double, p(d | c)
     
+    global A_MIN A_MAX B_MIN B_MAX P_1 P_2 P_3 N M
+    if all(c >= 0) && all(c <= A_MAX + B_MAX) %&& ...
+            %all(d >= c) && all(d <= 2 * c)
+        %res = nchoosek(c, d - c) * P_3 ^ (d - c) * (1 - P_3) ^ (2 * c - d);
+        res = binopdf(c, d - c, P_3);
+        res(d < c) = 0;
+        rec(d > 2 * c) = 0;
+    end
+end
+
+function res = p_c(c)
+% This function returns the probability p(c)
+% INPUT:
+%    c: int
+%
+% OUTPUT:
+%    res: double, p(c)
+
+    global A_MIN A_MAX B_MIN B_MAX P_1 P_2 P_3 N M
     res = 0;
-    if (c >= 0) && (c <= A_MAX + B_MAX) && (d >= c) && (d <= 2 * c)
-        res = nchoosek(c, d - c) * P_3 ^ (d - c) * (1 - P_3) ^ (2 * c - d);
+    if (c >= 0) && (c <= A_MAX + B_MAX)
+        p_a_b = p_c_cond_a_b(c, [A_MIN : A_MAX], [B_MIN : B_MAX]);
+        p_d_c = p_d_cond_c([c : 2 * c], c);
+        for d = c : 2 * c
+            for a = A_MIN : A_MAX
+                for b = B_MIN : B_MAX
+                    % res = res + p_c_cond_a_b(c, a, b);
+                    res = res + p_a_b(a - A_MIN + 1, b - B_MIN + 1);
+                end
+            end
+            res = res * p_d_c(d - c + 1);
+        end
+        res = res / ((A_MAX - A_MIN + 1) * (B_MAX - B_MIN + 1));
+    end
+end
+
+function res = p_d(d)
+% This function returns the probability p(d)
+% INPUT:
+%    d: int
+%
+% OUTPUT:
+%    res: double, p(d)
+
+    global A_MIN A_MAX B_MIN B_MAX P_1 P_2 P_3 N M 
+    res = 0;
+    if (d >= 0) && (d <= 2 * (A_MAX + B_MAX))
+%         d_cond_c = zeros(1, A_MAX + B_MAX + 1);
+%         for c = 0 : A_MAX + B_MAX
+%             d_cond_c(c + 1) = p_d_cond_c(d, c);
+%         end
+        d_cond_c = p_d_cond_c(d, [0 : A_MAX + B_MAX])
+%         for a = A_MIN : A_MAX
+%             for b = B_MIN : B_MAX
+%                 for c = 0 : (a + b)
+%                     res = res + p_c_cond_a_b(c, a, b) * d_cond_c(c + 1);
+%                 end
+%             end
+%         end
+        c_count = repmat([A_MIN : A_MAX]', 1, M) + ...
+            repmat([B_MIN : B_MAX], N, 1);
+        for c = 0 : (A_MAX + B_MAX)
+            res = res + sum(sum(p_c_cond_a_b(c, [A_MIN : A_MAX], ...
+                [B_MIN : B_MAX]))) * d_cond_c(c + 1);
+        end
+        
+        res = res / ((A_MAX - A_MIN + 1) * (B_MAX - B_MIN + 1));
     end
 end
